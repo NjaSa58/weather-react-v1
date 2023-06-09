72fed447da9ff71991cf5b623d2f43e35aac8088
juri006 <58621975+juri006@users.noreply.github.com>
2023-06-09 11:26:48 +0200

    Added ColorRing Loader

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { ColorRing } from "react-loader-spinner";
 
 export default function Weather(props) {
   function handleResponse(response) {
@@ -12,5 +13,17 @@ export default function Weather(props) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+    </div>
+  );
 }
