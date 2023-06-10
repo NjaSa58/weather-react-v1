b4c0723169d304a888ea9fe53512e0aacf6fb58a
juri006 <58621975+juri006@users.noreply.github.com>
2023-06-10 16:34:32 +0200

    Added fotter

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
         <h1>Weather App</h1>
         <Weather city="Zagreb" />
       </header>
+      <fotter>
+        <p>
+          This project has been created by Sanja Plazonić, and it is {""}
+          <a href="https://github.com/NjaSa58/weather-react-v1" target="_blank">
+            open-sourced on GitHub.
+          </a>
+        </p>
+      </fotter>
     </div>
   );
 }
